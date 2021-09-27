bd3bd4a0c1a11787a6843c2d3ff640f75a4d189b
IvanSerov <VanSer1989@gmail.com>
2021-09-27 09:59:31 +0200

    4ast'  1

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import 'bootstrap/dist/css/bootsrap.css';
+import '.index.css';
+import App from './components/app/';
+
+ReactDOM.render(<App />, document.getElementById('root'));
